05270952d4889588dca9372dd1740812d6693eea
PBillingsby <philbillingsby@gmail.com>
2024-08-30 13:49:15 -0400

    remove hard coded values

--- app/components/SendTransaction.tsx
+++ app/components/SendTransaction.tsx
@@ -6,11 +6,9 @@ import { useDebounce } from "use-debounce";
 import { parseEther } from "viem";
 
 const SendTransaction = () => {
-  const [address, setAddress] = useState("0x8bdCE5551B544AF8dFfB09Ff34c34da7FC241Bd0");
-  const [amount, setAmount] = useState("0.01");
+  const [address, setAddress] = useState("");
+  const [amount, setAmount] = useState("");
 
-  // Note: The debounced values are for UI purposes, such as validation or UI updates,
-  // and shouldn't be used directly for sending transactions on form submit.
   const [debouncedAddress] = useDebounce(address, 500);
   const [debouncedAmount] = useDebounce(amount, 500);
 
@@ -18,10 +16,9 @@ const SendTransaction = () => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // Use the actual input values, not the debounced ones, for sending transactions.
     sendTransaction({
-      to: debouncedAddress as `0x${string}`, // Make sure this is the recipient's address
-      value: parseEther(debouncedAmount) // Convert the amount to the necessary format
+      to: debouncedAddress as `0x${string}`,
+      value: parseEther(debouncedAmount)
     });
   };
 
